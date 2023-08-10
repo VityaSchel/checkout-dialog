f846c0b042090286eff811c45ee3319e71362f7a
hloth <git@hloth.dev>
2023-08-10 20:29:38 +0400

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { CheckoutModal } from 'checkout-modal'
+import type { CheckoutModalRef } from 'checkout-modal'
+import 'checkout-modal/style.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+function App() {
+  const checkoutRef = React.useRef<CheckoutModalRef>()
+
+  React.useEffect(() => {
+    checkoutRef.current?.open({
+      paymentInfo: {
+        title: 'Оплата подписки "название магазина"',
+        priceString: '999 ₽',
+        priceInRub: 999
+      },
+      // paymentProcessor: { name: 'cloudpayments', publicId: 'test_api_000000000000000002' },
+      paymentProcessor: { name: 'payselection', publickey: '0405397f7577bd835210a57708aafe876786dc8e2d12e6880917d61a4ad1d03a75068ea6bc26554c7a1bf5b50ed40105837eee001178579279eca57f89bdff5fc2' },
+      checkboxes: [
+        { defaultActive: true, htmlLabel: 'Foo bar' },
+        { defaultActive: true, htmlLabel: 'Hello world' },
+      ]
+    }, alert)
+  }, [])
+
+  return (
+    <CheckoutModal
+      ref={checkoutRef}
+    />
+  )
+}
